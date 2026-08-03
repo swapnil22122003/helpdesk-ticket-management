f26218a63403e6137996743ebef3bf87c13273d9
Swapnil Kulkarni <kulkarniswapnil728@gmail.com>
2026-08-03 22:37:58 +0530

    Initial Spring Boot setup with hello API

---
+++ src/main/java/com/gtl/helpdesk/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.gtl.helpdesk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Welcome to IT Helpdesk System";
+    }
+}   
