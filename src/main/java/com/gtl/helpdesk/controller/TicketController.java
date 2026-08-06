fed3c9fae283662f865cabf6daca9fe2871a26d6
Swapnil Kulkarni <kulkarniswapnil728@gmail.com>
2026-08-06 22:59:43 +0530

    Reference CRUD operation using service layer

--- src/main/java/com/gtl/helpdesk/controller/TicketController.java
+++ src/main/java/com/gtl/helpdesk/controller/TicketController.java
@@ -1,7 +1,7 @@
 package com.gtl.helpdesk.controller;
 
 import com.gtl.helpdesk.model.Ticket;
-import com.gtl.helpdesk.repository.TicketRepository;
+import com.gtl.helpdesk.service.TicketService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,53 +10,35 @@ import java.util.List;
 @RequestMapping("/api/tickets")
 public class TicketController {
 
-    private final TicketRepository ticketRepository;
 
-    public TicketController(TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
     }
 
     @PostMapping
     public Ticket createTicket(@RequestBody Ticket ticket) {
-        return ticketRepository.save(ticket);
+        return ticketService.createTicket(ticket);
     }
 
     @GetMapping
     public List<Ticket> getAllTickets() {
-        return ticketRepository.findAll();
+        return ticketService.getAllTickets();
     }
 
     @GetMapping("/{id}")
     public Ticket getTicketById(@PathVariable Long id) {
-        return ticketRepository.findById(id).orElse(null);
+        return ticketService.getTicketById(id).orElse(null);
     }
 
     @PutMapping("/{id}")
     public Ticket updateTicket(@PathVariable Long id, @RequestBody Ticket updatedTicket) {
-
-        Ticket ticket = ticketRepository.findById(id).orElse(null);
-
-        if (ticket == null) {
-            return null;
-        }
-
-        ticket.setTitle(updatedTicket.getTitle());
-        ticket.setDescription(updatedTicket.getDescription());
-        ticket.setStatus(updatedTicket.getStatus());
-        ticket.setPriority(updatedTicket.getPriority());
-
-        return ticketRepository.save(ticket);
+        return ticketService.updateTicket(id,updatedTicket);
     }
 
     @DeleteMapping("/{id}")
     public String deleteTicket(@PathVariable Long id) {
-
-        if (!ticketRepository.existsById(id)) {
-            return "Ticket not found";
-        }
-
-        ticketRepository.deleteById(id);
-
-        return "Ticket deleted successfully";
+        return ticketService.deleteTicket(id);
     }
 }
---
+++ src/main/java/com/gtl/helpdesk/service/TicketService.java
@@ -0,0 +1,53 @@
+package com.gtl.helpdesk.service;
+
+import com.gtl.helpdesk.model.Ticket;
+import com.gtl.helpdesk.repository.TicketRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+    public TicketService(TicketRepository ticketRepository)
+    {
+        this.ticketRepository= ticketRepository;
+    }
+
+    public Ticket createTicket(Ticket ticket){
+        return ticketRepository.save(ticket);
+    }
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Optional<Ticket> getTicketById( Long id) {
+        return ticketRepository.findById(id);
+    }
+
+    public Ticket updateTicket(Long id, Ticket updatedTicket){
+        Ticket ticket= ticketRepository.findById(id).orElse(null);
+        if(ticket==null){
+            return null;
+        }
+        ticket.setTitle(updatedTicket.getTitle());
+        ticket.setDescription(updatedTicket.getDescription());
+        ticket.setStatus(updatedTicket.getStatus());
+        ticket.setPriority(updatedTicket.getPriority());
+
+        return ticketRepository.save(ticket);
+    }
+
+    public String deleteTicket(Long id){
+        if (!ticketRepository.existsById(id)) {
+            return "Ticket not Found";
+        }
+
+        ticketRepository.deleteById(id);
+
+        return "Ticket deleted successfully";
+
+        }
+}
